3dbcff7d3aa86e0a63ecbd2e0d0c73f57cf2a8f5
ElerGard <48643205+ElerGard@users.noreply.github.com>
2021-06-26 21:51:26 +1000

    spaw /

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 function customAutoload(string $classN)
 {
-    $path = str_replace('game\\', DIRECTORY_SEPARATOR, $classN);
+    $path = str_replace('game//', DIRECTORY_SEPARATOR, $classN);
     require_once __DIR__ . "/src/$path.php";
 }
 
 spl_autoload_register('customAutoload');
 
-$game = new \game\game();
-$player = new \game\Player("Hop");
-$car = new \game\Car("BMW");
-$enemies = new \game\Enemies("Zombie");
+$game = new /game/game();
+$player = new /game/Player("Hop");
+$car = new /game/Car("BMW");
+$enemies = new /game/Enemies("Zombie");
 
 $player->action();
 $car->action();
